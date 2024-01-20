84a048e89a72fa155ddeead8e7df00dff15ccd99
Verso <vuoreol@proton.me>
2024-01-20 11:19:27 +0200

    Now

--- protest_app/settings.py
+++ protest_app/settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#    "/var/www/static/",
+#]
 
-STATIC_ROOT = "/var/www/static/"
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
